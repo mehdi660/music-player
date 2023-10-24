173b8ae2295f88e75b50ff17cf55462f9d5d15b5
mehdi660 <mehdi.belgoumrii@gmail.com>
2023-10-24 15:22:32 +0200

    link initialized

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,19 @@
 import React from "react";
 
 const Home = () => {
-    const _CLIENT_ID = "c6b658ec1b5544829e1049238a9b6d0a"
-    const _REDIRECT_URI = "http://localhost:3001"
-    const _AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
-
-
+  const CLIENT_ID = "c6b658ec1b5544829e1049238a9b6d0a";
+  const REDIRECT_URI = "http://localhost:3001";
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+  const REPONSE_TYPE = "token";
 
   return (
     <div>
       <h1>Hello world</h1>
-      <a href={ }>Login to spotify</a>
+      <a
+        href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&reponse_type=${REPONSE_TYPE}`}
+      >
+        Login to spotify
+      </a>
     </div>
   );
 };
