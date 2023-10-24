faae5227a145948bd76502d0284303a3e46e0cd2
mehdi660 <mehdi.belgoumrii@gmail.com>
2023-10-24 14:56:44 +0200

    file created

---
+++ src/Router.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
+const Router = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default Router;
