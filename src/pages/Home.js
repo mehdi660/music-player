d75a0271a7dc05d2f8ffd99094dca9bddcba60fe
mehdi660 <mehdi.belgoumrii@gmail.com>
2023-10-24 15:18:02 +0200

    client id add

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Home = () => {
-  return <div></div>;
+    const _CLIENT_ID = "c6b658ec1b5544829e1049238a9b6d0a"
+    const _REDIRECT_URI = "http://localhost:3001"
+    const _AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+
+
+
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <a href={ }>Login to spotify</a>
+    </div>
+  );
 };
 
 export default Home;
