a696125b47e0ded50c2d021498b0e699c3832c36
mehdi660 <mehdi.belgoumrii@gmail.com>
2023-10-25 15:57:46 +0200

    route created

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./home";
+import Feed from "./feed";
+import Player from "./player";
+import Trending from "./pages/Trending";
 
 const Router = () => {
   return (
@@ -8,6 +11,9 @@ const Router = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/feed" element={<Feed />} />
+          <Route path="/player" element={<Player />} />
+          <Route path="/trending" element={<Trending />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/player.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const player = () => {
+  return <div>player</div>;
+};
+
+export default player;
