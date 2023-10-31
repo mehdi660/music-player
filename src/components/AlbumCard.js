eb01ebd1215d51dd13a3a8583828fa75d2393a0d
mehdi660 <mehdi.belgoumrii@gmail.com>
2023-10-31 16:22:22 +0100

    albumPage's style in progress

--- src/components/AlbumCard.js
+++ src/components/AlbumCard.js
@@ -7,9 +7,11 @@ const AlbumCard = ({ album }) => {
 
   return (
     <div className="album">
-      <img src={album.images[1].url} alt={album.name} />
-      <h3>{album.name}</h3>
-      <h3>{album.type}</h3>
+      <div className="album-ctnr">
+        <img src={album.images[1].url} alt={album.name} />
+        <h3>{album.name}</h3>
+        <h3>{album.type}</h3>
+      </div>
     </div>
   );
 };
