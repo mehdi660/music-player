8e6228c381887614499340b419b11b1b400e50a6
mehdi660 <mehdi.belgoumrii@gmail.com>
2023-10-31 15:48:17 +0100

    album page in progress

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,39 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import axios from "axios";
 
 const Album = () => {
   let token = window.localStorage.getItem("token");
   const id = window.location.pathname.split("/")[2];
+  const [artistName, setArtistName] = useState(""); // Nouvel état pour stocker le nom de l'artiste
 
-  const getAlbum = async (e) => {
-    e.preventDefault();
-    try {
-      const { data } = await axios.get(
-        `https://api.spotify.com/v1/artists/${id}/albums`,
-        {
-          headers: { Authorization: `Bearer ${token}` },
-          params: {
-            type: "album",
-            limit: 5,
-          },
-        }
-      );
-      console.log(data);
-    } catch (error) {
-      // Gérer les erreurs de l'API ici, par exemple, afficher un message d'erreur à l'utilisateur
-      console.error("Erreur lors de la recherche d'artistes :", error);
-      // Vous pouvez également afficher un message d'erreur à l'utilisateur ou effectuer d'autres actions ici
-    }
-  };
-  console.log(id);
+  useEffect(() => {
+    const getAlbum = async () => {
+      try {
+        const { data } = await axios.get(
+          `https://api.spotify.com/v1/artists/${id}/albums`,
+          {
+            headers: { Authorization: `Bearer ${token}` },
+            params: {
+              type: "album",
+              limit: 5,
+            },
+          }
+        );
+        const firstAlbumId = data.items[0].id;
+        setArtistName(data.items[0].artists[0].name); // Mettez à jour le nom de l'artiste dans l'état
+        console.log(firstAlbumId);
+      } catch (error) {
+        console.error("Erreur lors de la recherche d'artistes :", error);
+      }
+    };
+
+    getAlbum(); // Appelez la fonction getAlbum dans useEffect pour qu'elle s'exécute après le rendu initial
+  }, [id, token]); // Assurez-vous d'inclure id et token dans la liste des dépendances
 
   return (
     <div>
       <NavLink to={"/"}>
         <p>Return to homepage</p>
       </NavLink>
-      <h1 onClick={getAlbum}>album</h1>
+      {/* Utilisez artistName dans votre JSX pour afficher le nom de l'artiste */}
+      <h1>Album of {artistName}</h1>
     </div>
   );
 };
