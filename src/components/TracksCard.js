d42054f89e2d0869cf5dd93ff1137482d966f86b
mehdi660 <mehdi.belgoumrii@gmail.com>
2023-11-21 17:32:00 +0100

    tracks style in progress

--- src/components/TracksCard.js
+++ src/components/TracksCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaHeart } from "react-icons/fa";
 
 const TracksCard = ({ data }) => {
   if (!data) {
@@ -6,13 +7,16 @@ const TracksCard = ({ data }) => {
   }
 
   const trackList = data.items.map((track) => (
-    <li key={track.id}>{track.name}</li>
+    <li key={track.id}>
+      {track.name} <FaHeart />
+    </li>
   ));
 
   return (
     <>
       <h1>Album track of "{data.items[0].name}"</h1>
       <ul>{trackList}</ul>
+      <button>Want to listen this album ? Click on me !</button>
     </>
   );
 };
