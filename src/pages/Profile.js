e464946462078b5e2a878573945469b4c15718c3
mehdi660 <mehdi.belgoumrii@gmail.com>
2023-11-24 19:06:35 +0100

    user info displayed

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
-
+import axios from "axios";
 const Profile = () => {
+  const token = localStorage.getItem("token");
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    const getProfile = async () => {
+      try {
+        const { data } = await axios.get("https://api.spotify.com/v1/me", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        setData(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getProfile();
+  }, [token]);
   return (
     <>
       <Header />
       <main>
-        <h1>Profile page</h1>
+        <h1>Profile</h1>
+        <p>Your name is {data.display_name}</p>
       </main>
     </>
   );
 };
-
 export default Profile;
